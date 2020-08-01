fac7b819600c7b266e32cc2992298eea996afa78
Rafael Morán <rm.veiga26@gmail.com>
2020-08-01 14:34:25 -0300

    peca/models.py - Criado model para peças

--- peca/models.py
+++ peca/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Peca(models.Model):
+    descricao = models.CharField(max_length=100, verbose_name='Descrição')
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.descricao
