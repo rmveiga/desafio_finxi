3bb374a17431dbcb790eff8c29d671d5ba520154
Rafael Morán <rm.veiga26@gmail.com>
2020-08-01 15:07:07 -0300

    peca/models.py - Removido o campo Usuário do model

--- peca/models.py
+++ peca/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 
 class Peca(models.Model):
     descricao = models.CharField(max_length=100, verbose_name='Descrição')
-    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.descricao
--- peca/viewsets.py
+++ peca/viewsets.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import Peca
 from .serializers import PecaSerializer
 
+
 class PecaViewSet(viewsets.ModelViewSet):
     queryset = Peca.objects.all()
     serializer_class = PecaSerializer
