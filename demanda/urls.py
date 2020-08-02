c6aed728051bf6972971198bc69dd79f06155976
Rafael Morán <rm.veiga26@gmail.com>
2020-08-02 17:49:26 -0300

    demanda/urls.py - Ajustado endpoint de demandas
    demanda/viewsets.py
    - Implementada função de listagem de demandas de acordo com o usuário que solicita os dados e com seu nível de permissão

--- demanda/urls.py
+++ demanda/urls.py
@@ -3,4 +3,4 @@ from rest_framework import routers
 from .viewsets import DemandaViewSet
 
 demanda_router = routers.DefaultRouter()
-demanda_router.register('demandas', DemandaViewSet)
+demanda_router.register('demandas', DemandaViewSet, basename='demanda-list')
--- demanda/viewsets.py
+++ demanda/viewsets.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Demanda
 from .serializers import DemandaSerializer
 
 
 class DemandaViewSet(viewsets.ModelViewSet):
-    queryset = Demanda.objects.all()
-    serializer_class = DemandaSerializer
+    def list(self, request, *args, **kwargs):
+        usuario = request.user
+        if usuario.is_superuser:
+            queryset = Demanda.objects.all()
+        elif usuario.is_staff:
+            queryset = Demanda.objects.filter(anunciante=usuario)
+        else:
+            return Response({'Acesso negado': 'Você não têm permissão para visualizar estes dados'})
+
+        serializer = DemandaSerializer(queryset, many=True)
+        return Response(serializer.data)
